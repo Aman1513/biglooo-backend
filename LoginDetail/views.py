ba4c040ce719e06d03967081c9c3c42c8a1abcc7
Aman1513 <54748176+Aman1513@users.noreply.github.com>
2022-10-30 19:38:34 +0530

    accenture

--- LoginDetail/views.py
+++ LoginDetail/views.py
@@ -1,11 +1,11 @@
 from .models import LoginDetail
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from .serializer import LoginDetailSerializers
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
-class LoginDetailList(ReadOnlyModelViewSet):
+class LoginDetailList(ModelViewSet):
     queryset = LoginDetail.objects.all()
     serializer_class = LoginDetailSerializers 
     filter_backends = (DjangoFilterBackend,SearchFilter)
